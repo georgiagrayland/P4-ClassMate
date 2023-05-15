35cd9b04d5b27f642784b7d7c833667400dcdf2d
georgiagrayland <georgiamayeva@live.co.uk>
2023-05-15 15:57:01 +0000

    add options for some data in the school model for the backend

--- website/models.py
+++ website/models.py
@@ -2,15 +2,38 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+region_options = (
+    ("London & SE"),
+    ("Manchester & NE"),
+    ("Bristol & SW"),
+    ("Newcastle & NE"),
+)
+
+status_options = (
+    ("Fee-paying"),
+    ("State Grammar"),
+    ("Academy"),
+)
+
+gender_options = (
+    ("Co-educational"),
+    ("Girls"),
+    ("Boys"),
+)
+
 
 # Create your models here.
 class School(models.Model):
+    """
+    School model, which stores all information about the schools
+    to be displayed on various pages of the website
+    """
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=500, unique=True)
     slug = models.SlugField(max_length=500, unique=True)
-    region = models.CharField(max_length=200)
-    status = models.CharField(max_length=200)
-    gender_type = models.CharField(max_length=200)
+    region = models.CharField(max_length=200, choices=region_options)
+    status = models.CharField(max_length=200, choices=status_options)
+    gender_type = models.CharField(max_length=200, choices=gender_options)
     class_size = models.IntegerField(max_length=3)
     avg_maths_grade = models.IntegerField(max_length=2)
     avg_english_grade = models.IntegerField(max_length=2)
