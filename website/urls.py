b621a639735cdd898229ec6bbd773c3944102982
georgiagrayland <georgiamayeva@live.co.uk>
2023-05-17 13:57:11 +0000

    edit url path for school detail page

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home')
+    path('', views.Home.as_view(), name='home'),
+    # path('schools_list', views.SchoolsList().as_view, name='schools'),
+    path('<slug:slug>/', views.SchoolDetail.as_view(), name='school_detail'),
 ]
