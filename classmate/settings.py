35c16305aa3fab0db9116b203592aa052fbd2844
georgiagrayland <georgiamayeva@live.co.uk>
2023-05-28 17:46:55 +0000

    deployment commit

--- classmate/settings.py
+++ classmate/settings.py
@@ -28,9 +28,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['class-mate.herokuapp.com', 'localhost', '8000-georgiagray-p4classmate-v5nk23k139t.ws-eu97.gitpod.io']
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
+ALLOWED_HOSTS = ['class-mate.herokuapp.com', 'localhost']
 
 
 # Application definition
