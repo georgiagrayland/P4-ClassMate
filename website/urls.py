90131f4508d9c71a2a7823bae6c920235b1cd3c8
georgiagrayland <georgiamayeva@live.co.uk>
2023-05-28 13:31:13 +0000

    edit url file so lines are not too long

--- website/urls.py
+++ website/urls.py
@@ -4,10 +4,14 @@ from django.urls import path
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
     path('schools/', views.schools_list, name='schools'),
-    path('schools/<slug:slug>/', views.SchoolDetail.as_view(), name='school_detail'),
+    path(
+        'schools/<slug:slug>/',
+        views.SchoolDetail.as_view(), name='school_detail'),
     path('comments/', views.my_comments, name='my_comments'),
     path(
-        'comments/edit/<int:comment_id>/', views.edit_comment, name='edit_comment'),
+        'comments/edit/<int:comment_id>/',
+        views.edit_comment, name='edit_comment'),
     path(
-        'comments/delete/<int:comment_id>/', views.delete_comment, name='delete_comment'),
+        'comments/delete/<int:comment_id>/',
+        views.delete_comment, name='delete_comment'),
 ]
