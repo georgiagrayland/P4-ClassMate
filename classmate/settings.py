445faa073167685465a95543f5f5010ac0e6486d
georgiagrayland <georgiamayeva@live.co.uk>
2023-05-29 10:13:08 +0000

    edit comments pages

--- classmate/settings.py
+++ classmate/settings.py
@@ -28,7 +28,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
